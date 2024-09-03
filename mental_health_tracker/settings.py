3fb496fb282937a0b677abceff48496afafce762
oscarryanda <scarryanda@gmail.com>
2024-09-03 19:05:41 +0700

    tes

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rhj9&z2&w&qj)ekt-z^lcz28vdp2%=*+ygo@c2e(9ppf!=k$6#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "oscar-ryanda-mentalhealthtracker..pbp.cs.ui.ac.id"]
 
 
 # Application definition
