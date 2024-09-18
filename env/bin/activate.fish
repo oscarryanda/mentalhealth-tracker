6f51d5de2c9df26bdd7be0c4f6d095c7fb8c8fd4
oscarryanda <scarryanda@gmail.com>
2024-09-18 13:32:55 +0700

    tutorial 3

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/scararthur/Downloads/Matkul/mental-health-tracker/env"
+set -gx VIRTUAL_ENV "/Users/scararthur/Downloads/Matkul/mentalhealth-tracker/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
